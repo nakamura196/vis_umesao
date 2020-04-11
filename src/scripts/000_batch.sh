70cb1980dd48c25222c37abbcd8513a343cf29c3
Satoru Nakamura <na.kamura.1263@gmail.com>
2020-04-12 07:52:53 +0900

    fix minor bug

--- src/scripts/000_batch.sh
+++ src/scripts/000_batch.sh
@@ -5,9 +5,10 @@ python 004_get_metadata.py
 python 005_get_color_data.py
 python 006_get_colors.py
 
-python 007_stitch_images.py ../data/ /Users/nakamura/git/thumbnail/umesao/ ../../docs/img/ 100 10 10 default 50 20 3 30000
-python 007_stitch_images.py ../data/ /Users/nakamura/git/thumbnail/umesao/ ../../docs/img/ 100 10 10 資料種別 50 20 3 30000
-python 007_stitch_images.py ../data/ /Users/nakamura/git/thumbnail/umesao/ ../../docs/img/100 10 10 colors 50 20 3 30000
+python 007_stitch_images.py ../data/ /Users/nakamura/git/thumbnail/umesao/ ../../docs/img/ 50 20 20 default 100 10 3 30000
+python 007_stitch_images.py ../data/ /Users/nakamura/git/thumbnail/umesao/ ../../docs/img/ 50 20 20 colors 100 10 3 30000
+python 007_stitch_images.py ../data/ /Users/nakamura/git/thumbnail/umesao/ ../../docs/img/ 50 20 20 資料種別 100 10 3 30000
+
 
 python 008_generate_metadata.py
 python 009_generate_labels.py ../data/ ../../docs/js/labels.json 100 10 50 20 3
